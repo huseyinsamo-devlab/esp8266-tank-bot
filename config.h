d1f9e80cb934e00f6eb1ec7fc3a28213f3996327
Hüseyin Samo <samo.huseyin.hs@gmail.com>
2025-11-04 23:28:17 +0300

    Create config.h

---
+++ config.h
@@ -0,0 +1,15 @@
+#pragma once
+// Wi-Fi
+#define WIFI_SSID     "YOUR_SSID"
+#define WIFI_PASS     "YOUR_PASSWORD"
+
+// Motor pinleri (L298N gibi sürücü)
+#define IN1  5   // D1
+#define IN2  4   // D2
+#define IN3  0   // D3
+#define IN4  2   // D4
+#define EN_LEFT  14 // D5 (PWM)
+#define EN_RIGHT 12 // D6 (PWM)
+
+// Varsayılan hız (0-1023)
+#define DEFAULT_PWM 700
